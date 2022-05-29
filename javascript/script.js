117feaaf476490260852f51728c4b91be42f3938
Venkat-aero <102429012+Venkat-aero@users.noreply.github.com>
2022-05-29 16:44:37 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,35 @@
 let days = 1;
 switch (days)
-// for sunday 
+
 {
   case 1:
     console.log("Sunday");
     break;
-// for monday
 
   case 2:
     console.log("Monday");
     break;
-// for tuesday
+
   case 3:
     console.log("Tuesday");
     break;
-// for wednesday
+
   case 4:
     console.log("Wednesday");
     break;
-// for thursday
+
   case 5:
     console.log("Thursday");
     break;
-// for friday
+
   case 6:
     console.log("Friday");
     break;
-// for saturday
+
     case 7:
         console.log("Saturday");
         break;
-    // if the given input is not valid 
+    
   default:
     console.log("Sorry it's not valid, why don't you try between the numbers  1 - 7 !");
     break;
