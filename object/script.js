1721e4832c781aba7a772b6ef46fc1792eeac0f5
Venkat-aero <102429012+Venkat-aero@users.noreply.github.com>
2022-05-29 17:10:51 +0530

    Create script.js

---
+++ object/script.js
@@ -0,0 +1,9 @@
+let length = 10;
+let breadth =20;
+if (length != breadth) 
+{console.log("As per the given dimensions, the object is Rectangle. ");
+}
+   
+ else {console.log("As per the given dimensions, the object is Square.");
+    
+}
