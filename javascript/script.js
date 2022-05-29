ba74549623d7e0beff0b6cd2496c52abf8a44fbc
Venkat-aero <102429012+Venkat-aero@users.noreply.github.com>
2022-05-29 16:26:26 +0530

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+let days = 0;
+switch (days) {
+  case 1:
+    console.log("Sunday");
+    break;
+
+
+  case 2:
+    console.log("Monday");
+    break;
+
+  case 3:
+    console.log("Tuesday");
+    break;
+
+  case 4:
+    console.log("Wednesday");
+    break;
+
+  case 5:
+    console.log("Thursday");
+    break;
+
+  case 6:
+    console.log("Friday");
+    break;
+
+    case 7:
+        console.log("Saturday");
+        break;
+  default:
+    console.log("Sorry it's not valid, why don't you try between the numbers  1 - 7 !");
+    break;
+}
